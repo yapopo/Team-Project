34f6bd42a829f56d6dca59108a3186afe37678c1
yapopo <dbrkdhk4120@naver.com>
2023-08-13 02:16:15 +0900

    ⚡️ footer 추가

--- subpage_3/script.js
+++ subpage_3/script.js
@@ -5,3 +5,35 @@ $(".gnb-list").hover(function(){
   $(this).find("ul").stop().slideUp();
   $(this).find("ul").removeClass("active")
 })
+
+$(document).ready(function(){
+  $('.banner-site-list').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
+  $('.play').click(function(){
+    $('.banner-site-list').slick('slickPlay');
+    
+  }); 
+  $('.stop').click(function(){
+    $('.banner-site-list').slick('slickPause');
+
+  });
+
+})
+
+$(".stop").click(function(){
+  $(".first").css("display","none")
+  $(".second").css("display","block")
+})
+
+$(".play").click(function(){
+  $(".second").css("display","none")
+  $(".first").css("display","block")
+})
