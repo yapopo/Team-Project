d6ed55fe051cc0ee6206603f7cbb12a1ea35884c
yapopo <dbrkdhk4120@naver.com>
2023-08-13 14:55:11 +0900

    ⚡️ slide section 마크업+스타일링+스크립트구현

--- subpage_5/script.js
+++ subpage_5/script.js
@@ -1,3 +1,6 @@
+
+// gnb-list 스트립트
+
 $(".gnb-list").hover(function(){
   $(this).find("ul").stop().slideDown();
   $(this).find("ul").addClass("active")
@@ -6,10 +9,14 @@ $(".gnb-list").hover(function(){
   $(this).find("ul").removeClass("active")
 })
 
+// sub-title 클릭과 호버 스크립트
+
 $(".sub-title1 ul li").click(function(){
   $(".sub-title1 ul li").removeClass("active")
   $(this).addClass("active")
 })
+
+// info 클릭 스크립트
 
 $(".info-list li").click(function(){
   $(".info-list li").removeClass("active")
@@ -22,6 +29,48 @@ $(".info-list li").click(function(){
     $(".info-content").css("display","none")
   }
 })
+
+// slide 스크립트
+
+$(document).ready(function(){
+  $('.slide-imgs').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    dots : true,
+    appendDots : $(".num"),
+    autoplaySpeed: 4000,
+    pauseOnHover : true,
+    prevArrow: $('#prev'),
+    nextArrow: $('#next'),
+  });
+
+  $('#play').click(function(){
+    $('.slide-imgs').slick('slickPlay');
+    
+  }); 
+  $('#pause').click(function(){
+    $('.slide-imgs').slick('slickPause');
+
+  });
+})
+
+$(".slick-dots li button").attr("disabled",false)
+
+
+$("#pause").click(function(){
+  $("#pause").css("display","none")
+  $("#play").css("display","block")
+})
+
+$("#play").click(function(){
+  $("#play").css("display","none")
+  $("#pause").css("display","block")
+})
+
+
+// footer 스크립트
 
 $(document).ready(function(){
   $('.banner-site-list').slick({
@@ -54,3 +103,6 @@ $(".play").click(function(){
   $(".second").css("display","none")
   $(".first").css("display","block")
 })
+
+
+
