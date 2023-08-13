451faaf81f07c64dfca6b59e671192cd73d0722b
yapopo <dbrkdhk4120@naver.com>
2023-08-13 10:50:16 +0900

    ⚡️ footer 삽입

--- subpage_5/script.js
+++ subpage_5/script.js
@@ -15,3 +15,35 @@ $(".info-list li").click(function(){
   $(".info-list li").removeClass("active")
   $(this).addClass("active")
 })
+
+$(document).ready(function(){
+  $('.banner-site-list').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
+  $('.play').click(function(){
+    $('.banner-site-list').slick('slickPlay');
+    
+  }); 
+  $('.stop').click(function(){
+    $('.banner-site-list').slick('slickPause');
+
+  });
+
+})
+
+$(".stop").click(function(){
+  $(".first").css("display","none")
+  $(".second").css("display","block")
+})
+
+$(".play").click(function(){
+  $(".second").css("display","none")
+  $(".first").css("display","block")
+})
