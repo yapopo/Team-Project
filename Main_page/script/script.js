74314b94afff0c736dae65a7f1d59aa7c2e76083
cksal8449 <cksal8449@naver.com>
2023-08-21 22:56:37 +0900

    📝 slick slider를 이용한 slide 기능 구현

---
+++ Main_page/script/script.js
@@ -0,0 +1,10 @@
+$('.slide-wrap').slick({
+    slidesToShow: 1,
+    dots:true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    fadeSpeed: 5000,
+    tocuMove:true,
+    appendDots: 'map-btn',
+});
