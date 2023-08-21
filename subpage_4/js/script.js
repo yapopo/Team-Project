1a6fb385366fc554e5da8acda7a32233d17aa119
yapopo <dbrkdhk4120@naver.com>
2023-08-22 02:31:30 +0900

    ✨ 수정된 header

--- subpage_4/js/script.js
+++ subpage_4/js/script.js
@@ -1,34 +1,43 @@
 // mainmenu
 $('.submenu-list').hide();
 
+$(".mainmenu-list").hover(function(){
+  $(".submenu-back").stop().slideDown(500);
+}, function(){
+  $(".submenu-back").stop().slideUp(500);
+})
+
 // 메인메뉴 hover시 폰트색깔, 이미지 바뀜
 $('.mainmenu-item').hover(function () {
-    $(this).find('.submenu-list').stop().slideDown(500);
-    $('.global-header').css('backgroundColor', '#fbfbfb');
-    $('.global-header').css('transition', 'none');
-    $('.top-menu, .top-menu a').css('color', '#000');
-    $(".top-right-menu img").attr("src", "../Project-images/header/topIcon.png");
-    $('.mainmenu').css('border-color','#bbb');
-    $('.mainmenu-item>a').css('color', '#000');
-    $('.mainmenu h1 span').css('color', '#000');
-    $(".mainmenu h1 img").attr("src", "../Project-images/header/logo.png");
-    $(".menu-buttons a, i").css('color', '#000');
-    $(".menu-buttons img").attr("src", "../Project-images/header/home-black2.png");
-    $(".submenu-list").css("border-top","1px solid #bbb")
-    $(".ul-wrapper ul li").stop().fadeIn();
+  $(this).find('.submenu-list').stop().slideDown(500);
+  $('.global-header').css('backgroundColor', '#fbfbfb');
+  $('.global-header').css('transition', 'none');
+  $('.top-menu, .top-menu a').css('color', '#000');
+  $(".menu-img img").attr("src", "../Project-images/header/topIcon.png");
+  $('.mainmenu').css('border-color','#bbb');
+  $('.mainmenu-item>a').css('color', '#000');
+  $('.mainmenu h1 span').css('color', '#000');
+  $(".mainmenu h1 img").attr("src", "../Project-images/header/logo.png");
+  $(".menu-buttons a, i").css('color', '#000');
+  $(".menu-buttons img").attr("src", "../Project-images/header/home-black2.png");
+  $(".submenu-list").css("border-top","1px solid #bbb")
+  $(this).find(".ul-wrapper").stop().fadeIn(1000);
+  $(this).find(".submenu-left-img").stop().fadeIn(1000);
+  $(".submenu-left-img p").text($(this).children("a").text())
 
 }, function () {
-    $(this).find('.submenu-list').stop().slideUp();
-    $('.global-header').css('backgroundColor', 'transparent');
-    $('.global-header').css('transition', '1000ms', 'ease-in-out');
-    $('.top-menu, .top-menu a').css('color', '#fff');
-    $(".top-right-menu img").attr("src", "../Project-images/header/topIcon_white.png");
-    $('.mainmenu-item>a').css('color', '#fff');
-    $('.mainmenu h1 span').css('color', '#fff');
-    $(".mainmenu h1 img").attr("src", "../Project-images/header/white-logo.png");
-    $(".menu-buttons a, i").css('color', '#fff');
-    $(".menu-buttons img").attr("src", "../Project-images/header/home-white.png");
-    $(".ul-wrapper ul li").stop().fadeOut();
+  $(this).find('.submenu-list').stop().slideUp();
+  $('.global-header').css('backgroundColor', 'transparent');
+  $('.global-header').css('transition', '1000ms', 'ease-in-out');
+  $('.top-menu, .top-menu a').css('color', '#fff');
+  $(".menu-img img").attr("src", "../Project-images/header/topIcon_white.png");
+  $('.mainmenu-item>a').css('color', '#fff');
+  $('.mainmenu h1 span').css('color', '#fff');
+  $(".mainmenu h1 img").attr("src", "../Project-images/header/white-logo.png");
+  $(".menu-buttons a, i").css('color', '#fff');
+  $(".menu-buttons img").attr("src", "../Project-images/header/home-white.png");
+  $(this).find(".ul-wrapper").stop().fadeOut(500);
+  $(this).find(".submenu-left-img").stop().fadeOut(500);
 });
 
 // gnb script
