a72f7c64e09c80af8811a580e3a907106629789b
cksal8449 <cksal8449@naver.com>
2023-09-02 19:14:10 +0900

    📝 tap기능 추가

--- Main_page/section2/script/script.js
+++ Main_page/section2/script/script.js
@@ -1,13 +1,24 @@
-// slick slider
-$('.slide-wrap').slick({
-    slidesToShow: 1,
-    dots:true,
+$(".slide-wrap1").slick({
+    dots: true,
     autoplay: true,
-    autoplaySpeed: 2000,
-    fade: true,
-    fadeSpeed: 5000,
-    tocuMove:true,
-    appendDots: 'map-btn',
+    autoplaySpeed: 3000,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.img-prev'),
+    nextArrow: $('.img-next'),
+    // fade 
+    // fade: true,
+    // fadeSpeed: 5000,
+
+    customPaging: function (slider, i) {
+        var title = $(slider.$slides[i]).data('title');
+        var num = i + 1;
+        return '<a class="item-dot"> <span>' + [i + 1] + '</span>' + title + ' </a>'
+
+    },
+
 });
 
 
@@ -16,11 +27,11 @@ let btn = $('.tab-btn');
 let btnlen = $('.tab-btn').length;
 let con = $('.tab-content');
 
-for(let i = 0; i < btnlen; i++){
-    btn.eq(i).on('click', function(){
+for (let i = 0; i < btnlen; i++) {
+    btn.eq(i).on('click', function () {
         btn.removeClass('active');
         btn.eq(i).addClass('active');
         con.removeClass('show')
         con.eq(i).addClass('show');
-        });
+    });
 }
