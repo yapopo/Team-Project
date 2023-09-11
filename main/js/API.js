a77b09283426e2a73b1501a8f18298659a601053
yapopo <dbrkdhk4120@naver.com>
2023-09-11 11:35:11 +0900

    ⚡️ 날씨 소수점 없애기

--- main/js/API.js
+++ main/js/API.js
@@ -4,7 +4,7 @@
 $.getJSON(`https://api.openweathermap.org/data/2.5/weather?q=Seoul&limit=5&appid=1255e4aac90af2ff4a1905e43962ab4b&units=metric`, function (result) {
    
     // 현재온도
-    $(".temp").append(result.main.temp); //현재온도
+    $(".temp").append(Math.ceil(result.main.temp)); //현재온도
 
     // 현재온도 아이콘
     let wiconUrl = '<img src="http://openweathermap.org/img/wn/' + result.weather[0].icon + '.png" alt="' + result.weather[0].description + '">';
