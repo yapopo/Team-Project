388b546b57b184b5cec602c15af4c79e8212a66a
Janeee205 <ga473625@gmail.com>
2023-09-11 23:22:23 +0900

    ✨ 프로젝트파일

--- express/loginPages/edit/script.js
+++ express/loginPages/edit/script.js
@@ -203,23 +203,24 @@ $(".play").click(function () {
 })
 
 
-
 /*********************
  * 회원가입 폼 script *
  ********************/
 
 
-let idveri = pwveri = pwchkveri = nameveri = genderveri = phoneveri = emailveri = false;
+let idveri = pwveri = pwchkveri = nameveri = emailveri = false;
 
 // Essention Infomation
 let essenInfo = '<span class="text-red"> 필수 정보입니다. </span>';
 
 
 // 아이디
-document.querySelector('.userid input').addEventListener('focusout', function () {
+// window.onload = function () {
+
+document.querySelector('.id input').addEventListener('focusout', function () {
   let userId = this.value;
   let idExp = /^[a-z0-9]{5,8}$/;
-  let idWarn = document.querySelector('.userid .warn');
+  let idWarn = document.querySelector('.id .warn');
 
   if (userId.length == 0) {
     // 필수 정보입니다.
@@ -233,6 +234,7 @@ document.querySelector('.userid input').addEventListener('focusout', function ()
   }
 })
 
+// }
 
 // 비밀번호
 let userPw = document.querySelector('.userpw input');
@@ -247,16 +249,16 @@ userPw.addEventListener('focusout', function () {
   if (userPwVal.length == 0) {
     pwWarn.innerHTML = essenInfo;
     pwText.innerHTML = '';
-    pwImg.src = 'images/signup/m_icon_pw_step_01.png';
+    pwImg.src = '../Project-images/login/m_icon_pw_step_01.png';
   } else if (!pwExp.test(userPwVal)) {
     pwWarn.innerHTML = '<span class="text-red"> 8~20자 영문 대 소문자, 숫자, 특수문자를 사용하세요. </span>';
     pwText.innerHTML = '<span class="text-red"> 사용불가 </span>';
-    pwImg.src = 'images/signup/m_icon_pw_step_10.png';
+    pwImg.src = '../Project-images/login/m_icon_pw_step_10.png';
   } else {
     pwveri = true;
     pwWarn.innerHTML = '';
     pwText.innerHTML = '<span class="text-green"> 안전 </span>';
-    pwImg.src = 'images/signup/m_icon_pw_step_04.png';
+    pwImg.src = '../Project-images/login/m_icon_pw_step_04.png';
   }
 })
 
@@ -271,18 +273,18 @@ document.querySelector('.userpw-chk input').addEventListener('focusout', functio
   if (userpwChk.length == 0) {
     pwChkWarn.innerHTML = essenInfo;
     pwChkText.innerHTML = '';
-    pwChkImg.src = 'images/signup/m_icon_pw_step_01.png';
+    pwChkImg.src = '../Project-images/login/m_icon_pw_step_01.png';
   } else if (userpwChk == userPw.value) {
     pwchkveri = true;
     pwChkWarn.innerHTML = '';
-    pwChkText.innerHTML = '<span class="text-green"> 안전 </span>';
-    pwChkImg.src = 'images/signup/m_icon_pw_step_07.png';
+    pwChkText.innerHTML = '<span class="text-green"> 일치 </span>';
+    pwChkImg.src = '../Project-images/login/m_icon_pw_step_07.png';
   } else {
     pwChkWarn.innerHTML = '<span class="text-red">비밀번호가 일치하지 않습니다.</span>';
-    pwChkImg.src = 'images/signup/m_icon_pw_step_02.png';
+    pwChkText.innerHTML = '';
+    pwChkImg.src = '../Project-images/login/m_icon_pw_step_02.png';
   }
 })
-
 
 // 이름
 document.querySelector('.username input').addEventListener('focusout', function () {
@@ -301,16 +303,6 @@ document.querySelector('.username input').addEventListener('focusout', function 
 })
 
 
-// 이메일
-// 다시 수정
-
-// 이메일 주소 가져오기
-// function getEmail() {
-//   var mailSelect = document.getElementById('select-mail');
-//   var mailSelecttextVal = mailSelect.value;
-//   let mail = mailSelecttextVal;
-// }
-
 document.querySelector('.usermail input').addEventListener('focusout', function () {
   let usermail = this.value;
   let mailWarn = document.querySelector('.usermail .warn');
@@ -328,107 +320,23 @@ document.querySelector('.usermail input').addEventListener('focusout', function 
 })
 
 
-// 성별
-let genderInputs = document.querySelectorAll('.gender input');
-
-genderInputs.forEach(function (item) {
-  item.addEventListener('click', function () {
-    this.querySelector('input[type="radio"]').checked = true;
-    genderveri = true;
-  })
-})
-
-
-// 휴대전화
-
-let numWarn = document.querySelector('.phone .warn');
-let phoneInputs = document.querySelectorAll('.phone input');
-
-$('.phone input').focusout(function () {
-  // console.log($('.phone2').val());
-
-  let phoneExp = /^\d{3,4}$/;;
-  let phone2Val = $('.phone2').val();
-  let phone3Val = $('.phone3').val();
-
-  // console.log(phone2Val, phone3Val);
-
-  if ($('.phone2').val().length == 0 || $('.phone3').val().length == 0) {
-    $('.phone .warn').html('<span class="text-red">필수 정보입니다.</span>');
-  } else if (!phoneExp.test(phone2Val) || !phoneExp.test(phone3Val)) {
-    $('.phone .warn').html('<span class="text-red"> 형식에 맞지않는 번호입니다. </span>');
-  } else {
-    $('.phone .warn').empty();
-    phoneveri = true;
-  }
-})
-
-
-
-
-/********************
- * 우편번호 api 연결 *
- *******************/
-
-function sample6_execDaumPostcode() {
-  new daum.Postcode({
-    oncomplete: function (data) {
-      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
-      // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-      // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-      var addr = ''; // 주소 변수
-      var extraAddr = ''; // 참고항목 변수
-
-      //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-      if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
-        addr = data.roadAddress;
-      } else { // 사용자가 지번 주소를 선택했을 경우(J)
-        addr = data.jibunAddress;
-      }
-
-      // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-      if (data.userSelectedType === 'R') {
-        // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-        // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-        if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-          extraAddr += data.bname;
-        }
-        // 건물명이 있고, 공동주택일 경우 추가한다.
-        if (data.buildingName !== '' && data.apartment === 'Y') {
-          extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-        }
-        // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-        if (extraAddr !== '') {
-          extraAddr = ' (' + extraAddr + ')';
-        }
-        // 조합된 참고항목을 해당 필드에 넣는다.
-        document.getElementById("sample6_extraAddress").value = extraAddr;
-
-      } else {
-        document.getElementById("sample6_extraAddress").value = '';
-      }
-
-      // 우편번호와 주소 정보를 해당 필드에 넣는다.
-      document.getElementById('sample6_postcode').value = data.zonecode;
-      document.getElementById("sample6_address").value = addr;
-      // 커서를 상세주소 필드로 이동한다.
-      document.getElementById("sample6_detailAddress").focus();
-    }
-  }).open();
-}
 
 
 // 가입하기 제출
-document.getElementById('submit').addEventListener('click', function (e) {
-  let isTrue = idveri && pwveri && pwchkveri && nameveri && genderveri && phoneveri && emailveri;
 
-  if (isTrue) {
-    document.getElementById('signup-form').submit();
-  } else {
+
+
+let submitButton = document.getElementById('submit')
+
+submitButton.addEventListener('click', function (e) {
+  let isTrue = idveri && pwveri && pwchkveri && nameveri && emailveri;
+
+  if (!isTrue) {
     e.preventDefault();
     document.querySelectorAll('input').forEach(function (input) {
       input.dispatchEvent(new Event('focusout'));
     })
   }
 })
+
+
---
+++ express/main/js/header_footer.js
@@ -0,0 +1,128 @@
+// mainmenu
+$('.submenu-list').hide();
+
+$(".mainmenu-list").hover(function(){
+  $(".submenu-back").stop().slideDown(500);
+}, function(){
+  $(".submenu-back").stop().slideUp(500);
+})
+
+// 메인메뉴 hover시 폰트색깔, 이미지 바뀜
+$('.mainmenu-item').hover(function () {
+    $(this).find('.submenu-list').stop().slideDown(500);
+    $('.global-header').css('backgroundColor', '#fbfbfb');
+    $('.global-header').css('transition', 'none');
+    $('.top-menu, .top-menu a').css('color', '#000');
+    $(".menu-img img").attr("src", "../Project-images/header/topIcon.png");
+    $('.mainmenu').css('border-color','#bbb');
+    $('.mainmenu-item>a').css('color', '#000');
+    $('.mainmenu h1 span').css('color', '#000');
+    $(".mainmenu h1 img").attr("src", "../Project-images/header/logo.png");
+    $(".menu-buttons a, i").css('color', '#000');
+    $(".menu-buttons img").attr("src", "../Project-images/header/home-black2.png");
+    $(".submenu-list").css("border-top","1px solid #bbb")
+    $(this).find(".ul-wrapper").stop().fadeIn(1000);
+    $(this).find(".submenu-left-img").stop().fadeIn(1000);
+    $(".submenu-left-img p").text($(this).children("a").text())
+
+}, function () {
+    $(this).find('.submenu-list').stop().slideUp();
+    $('.global-header').css('backgroundColor', 'transparent');
+    $('.global-header').css('transition', '1000ms', 'ease-in-out');
+    $('.top-menu, .top-menu a').css('color', '#fff');
+    $(".menu-img img").attr("src", "../Project-images/header/topIcon_white.png");
+    $('.mainmenu-item>a').css('color', '#fff');
+    $('.mainmenu h1 span').css('color', '#fff');
+    $(".mainmenu h1 img").attr("src", "../Project-images/header/white-logo.png");
+    $(".menu-buttons a, i").css('color', '#fff');
+    $(".menu-buttons img").attr("src", "../Project-images/header/home-white.png");
+    $(this).find(".ul-wrapper").stop().fadeOut(500);
+    $(this).find(".submenu-left-img").stop().fadeOut(500);
+});
+
+
+
+// 오늘 날짜 가져오기
+let date = new Date();
+let month = date.getMonth() + 1
+let day = date.getDate()
+
+// 요일넣기
+let todayweek = date.getDay();
+let week = ['일', '월', '화', '수', '목', '금', '토'];
+
+let todayLabel = week[todayweek];
+
+// '0' 붙여 두자릿수로 만들기
+if (month >= 10) {
+    mnum = month;
+} else {
+    mnum = '0' + month;
+}
+
+if (day >= 10) {
+    dnum = day;
+} else {
+    dnum = '0' + day;
+}
+
+$('.month').html(mnum);
+$('.date').html(dnum);
+$('.week').html(todayLabel);
+
+
+// 이미지 슬라이드
+// $('.slider > .img-item:gt(0)').hide();
+// setInterval(() => {
+//     $('.slider .img-item:first-child').fadeTo(1000, 0)
+//         .next('.img-item').fadeTo(1000, 1)
+//         .end(1000).appendTo('.slider')
+// }, 5000);
+
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade : true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  })
+});
+
+
+// footer 스크립트
+
+$(document).ready(function(){
+  $('.banner-site-list').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
+  $('.play').click(function(){
+    $('.banner-site-list').slick('slickPlay');
+    
+  }); 
+  $('.stop').click(function(){
+    $('.banner-site-list').slick('slickPause');
+
+  });
+
+})
+
+$(".stop").click(function(){
+  $(".first").css("display","none")
+  $(".second").css("display","block")
+})
+
+$(".play").click(function(){
+  $(".second").css("display","none")
+  $(".first").css("display","block")
+})
+
+
